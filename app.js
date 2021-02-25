1848b4d354a492f5740f9f89ab63e9159e9b93fe
sumit <sumitpatil90990@gmail.com>
2021-02-26 01:10:29 +0530

    added logic for custom city weather

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 const express = require('express');
-
+const bodyParser = require('body-parser');
 const https = require('https');
 
 const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", (req, res) => {
-  const url = "https://api.openweathermap.org/data/2.5/weather?appid=9838e3a6e740cfbe0dad83b3af679370&q=delhi&units=metric";
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", (req, res)=>{
+  const query = req.body.cityName
+  const apiKey = "9838e3a6e740cfbe0dad83b3af679370";
+  const unit = "metric"
+  const url = "https://api.openweathermap.org/data/2.5/weather?appid=" + apiKey + "&q=" + query + "&units=" +unit;
 
   https.get(url, function(response) {
     console.log(response.statusCode);
@@ -17,7 +25,7 @@ app.get("/", (req, res) => {
       const icon = weatherData.weather[0].icon;
       const imageurl = "http://openweathermap.org/img/wn/" +icon+ "@2x.png";
       res.write("<h1>Weather is " + weatherDescription + "</h1>");
-      res.write("<h1> The tempwrature in Delhi is" + temp + " degree Celcius </h1>");
+      res.write("<h1> The temprature in "+query+ " is " + temp + " degree Celcius </h1>");
       res.write("<img src="+imageurl+">");
       res.send();
     });
